00419ac9c77e0a823598fb0c40866ec96d8db4b3
Mahdi <88978694+darkkLUCIFER@users.noreply.github.com>
2022-09-24 11:29:10 +0330

    added login and protected to models.py

--- models.py
+++ models.py
@@ -1,10 +1,22 @@
 class Supervisor:
 
-    def __init__(self, username, password, phone_number):
+    def __init__(self, username, password, phone_number=None):
         self.username = username
         self.password = password
         self.phone_number = phone_number
+        self.__is_logged_in = False
 
     @classmethod
     def sample(cls):
         return cls('ali', '123', '09351234567')
+
+    @classmethod
+    def login(cls, username, password):
+        supervisor = cls(username, password)
+        supervisor.__is_logged_in = True
+        return supervisor
+
+    def protected(self):
+        if self.__is_logged_in:
+            return [1, 2, 3]
+        raise ValueError()
